3fa9b79e84e4f638e502226364e99af0f20c5673
가현 <bufar1014@gmail.com>
2026-04-23 11:53:46 +0900

    dto에 validation 추가

--- src/main/kotlin/com/example/demo/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/demo/member/controller/MemberController.kt
@@ -2,6 +2,7 @@ package com.example.demo.member.controller
 
 import com.example.demo.member.dto.MemberDto
 import com.example.demo.member.service.MemberService
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class MemberController(private val memberService: MemberService) {
     /* 회원 가입 */
     @PostMapping("/signup")
-    fun signup(@RequestBody memberDto: MemberDto) : String {
+    fun signup(@RequestBody @Valid memberDto: MemberDto) : String {
         return memberService.signup(memberDto)
     }
 }
